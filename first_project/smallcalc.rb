3ab77962cf370fc96dff25407f341b1a79ed6e34
Eric-Barajas <barajaseric505@gmail.com>
2023-05-18 14:29:49 -0700

    done

---
+++ first_project/smallcalc.rb
@@ -0,0 +1,16 @@
+puts "enter first number: "
+num1 = gets.chomp().to_f
+puts "enter opertaor: "
+op = gets.chomp()
+puts "enter second number: "
+num2 = gets.chomp().to_f
+
+if op == '+'
+    puts (num1 + num2)
+elsif op == '/'
+    puts (num1 / num2)
+elsif op == '*'
+    puts (num1 * num2)
+else
+    puts "Inavlid operator"
+end
